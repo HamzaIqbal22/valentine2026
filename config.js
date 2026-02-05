4bbb6135a4bda509ca4b3c12d61832e53ff15e78
Hamza <81776951+HamzaIqbal22@users.noreply.github.com>
2026-02-05 18:32:17 -0500

    Update Valentine's name and celebration messages

--- config.js
+++ config.js
@@ -4,8 +4,7 @@
 
 const CONFIG = {
     // Your Valentine's name that will appear in the title
-    // Example: "Delisha", "Anjitesh", "Mike"
-    valentineName: "Delisha",
+    valentineName: "Areej",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -35,7 +34,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
+            text: "Will you be my Valentine on February 13-14th, 2026? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -45,14 +44,14 @@ const CONFIG = {
     // They show up depending on how far they slide the meter
     loveMessages: {
         extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        high: "To the moon and back! 🚀💝",              // Shows when they go past 1000%
+        normal: "And through Jannah! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "LFG!!!! I'm the luckiest person in the world! 🎉💝💖💝💓",
+        message: "Now come get your gift, a big warm hug and a huge kiss! ",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
